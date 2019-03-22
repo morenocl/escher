c5083e64a80656627e0286accf3b7c09b3cee270
Leandro Acosta <leacosta97@gmail.com>
2019-03-22 10:12:29 -0300

    Add r180 and r270 combinators

--- Dibujo.hs
+++ Dibujo.hs
@@ -6,9 +6,14 @@ type Dibujo a = Basica a | Rotar a | Espejar a | Rot45 a
               | Juntar Int Int a a
               | Encimar a a
 
+-- composicion n-veces de una funcion
 comp :: (a -> a) -> Int -> a-> a
 comp f 0 a  = a 
 comp f c a = comp(f c-1 f(a))
 
+-- rotaciones de multiplos de 90
+r180 :: Dibujo a -> Dibujo -> a
+r180 a = Rotar $ Rotar a
 
-
+r270 :: Dibujo a -> Dibujo a
+r270 a = Rotar $ Rotar $ Rotar a
