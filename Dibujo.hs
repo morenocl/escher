5e57d52e038de690836722a17ef7e7dd878d30b3
gonzalo <gongigena@hotmail.com>
2019-03-22 10:35:53 -0300

    Add Ciclar Encimar4 Cuarteto

--- Dibujo.hs
+++ Dibujo.hs
@@ -31,3 +31,17 @@ r270 a = Rotar $ Rotar $ Rotar a
 -- Superpone una figura con otra
 (^^^) :: Dibujo a -> Dibujo a -> Dibujo a
 (^^^) a b = Encimar a b
+
+
+-- dada una figura la repite en cuatro cuadrantes
+cuarteto :: Dibujo a -> Dibujo a
+cuarteto a = Apilar (juntar a a) (Juntar a a)
+
+-- una figura repetida con las cuatro rotaciones, superimpuestas.
+encimar4 :: Dibujo a -> Dibujo a 
+encimar4 a = Encimar (Encimar (Rotar a) (Rotar $ Rotar a) ) (Encimar (Rotar $ Rotar $ Rotar a) a)
+
+-- cuadrado con la misma figura rotada $i$ por $90$ para $i \in \{1..3\}$.
+-- No confundir con encimar4!
+ciclar :: Dibujo a -> Dibujo a
+ciclar a = Apilar (Juntar  (Rotar a) a )  (Juntar (Rotar $ Rotar $ Rotar a) (Rotar $ Rotar a) )
