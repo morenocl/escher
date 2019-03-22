47d4435acad0f88ff4021181e07c6074d5ba3405
Christian L Moreno <christianluciano.m@gmail.com>
2019-03-22 09:44:43 -0300

    Definicion del lenguaje

--- Dibujo.hs
+++ Dibujo.hs
@@ -1,4 +1,7 @@
 module Dibujo where
 
 -- definir el lenguaje
-type Dibujo a = ()
+type Dibujo a = Basica a | Rotar a | Espejar a | Rot45 a
+              | Apilar Int Int a a
+              | Juntar Int Int a a
+              | Encimar a a
