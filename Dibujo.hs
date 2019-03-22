07a8e4b25d4da85a88e8928aa726efee8ab99baa
gonzalo <gongigena@hotmail.com>
2019-03-22 10:08:16 -0300

    Comp is implemented

--- Dibujo.hs
+++ Dibujo.hs
@@ -5,3 +5,10 @@ type Dibujo a = Basica a | Rotar a | Espejar a | Rot45 a
               | Apilar Int Int a a
               | Juntar Int Int a a
               | Encimar a a
+
+comp :: (a -> a) -> Int -> a-> a
+comp f 0 a  = a 
+comp f c a = comp(f c-1 f(a))
+
+
+
