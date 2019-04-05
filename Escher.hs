b476d39c3a65bc20d21026ae23103f3d874395ed
Christian L Moreno <christianluciano.m@gmail.com>
2019-04-05 11:28:23 -0300

    Implemantacion Escher

--- Escher.hs
+++ Escher.hs
@@ -7,19 +7,22 @@ type Escher = Bool
 
 -- el dibujo u
 dibujo_u :: Dibujo Escher -> Dibujo Escher
-dibujo_u p = undefined 
+dibujo_u p = rot45(ciclar p) 
 
 -- el dibujo t
 dibujo_t :: Dibujo Escher -> Dibujo Escher
-dibujo_t p = undefined 
+dibujo_t p = p ^^^ Rot45(p .-. p3)
+            where p3 = r270 p
 
 -- esquina con nivel de detalle en base a la figura p
 esquina :: Int -> Dibujo Escher -> Dibujo Escher
-esquina n p = undefined
+esquina 1 p = cuarteto blank blank blank (dibujo_u p)
+esquina n p = cuarteto (esquina n-1) (lado n-1 p) (Rotar (lado n-1 p)) (dibujo_u p)
 
 -- lado con nivel de detalle
 lado :: Int -> Dibujo Escher -> Dibujo Escher
-lado n p = undefined
+lado 1 p = cuarteto b b (Rotar p) p
+lado n p = cuarteto (lado (n-1) p) (lado (n-1) p) (Rotar p) p
 
 -- por suerte no tenemos que poner el tipo!
 noneto p q r s t u v w x = undefined
