4befaa494453b71b4eb5cab492cd5f14723076f6
Leandro Acosta <leacosta97@gmail.com>
2019-04-05 09:59:30 -0300

    Fix rotar, rotar45, espejar and add encimar

--- Interp.hs
+++ Interp.hs
@@ -74,8 +74,12 @@ rotar :: FloatingPic -> Vector -> Vector -> Vector -> Picture
 rotar p a b c = p (a V.+ b) c (V.negate b)
 
 rotar45 :: FloatingPic -> Vector -> Vector -> Vector -> Picture
-rotar45 p a b c = p (a V.+ (b V.+ c)V./2) ((b V.+ c) V./ 2) ((c V.- b) V./ 2)
+rotar45 p a b c = p (a V.+ (half (b V.+ c))) (half (b V.+ c)) (half (c V.- b))
 
 espejar :: FloatingPic -> Vector -> Vector -> Vector -> Picture
-espejar p a b c = p (a V.+ b) (v.negate b) c
+espejar p a b c = p (a V.+ b) (V.negate b) c
+
+encimar :: FloatingPic -> FloatingPic -> Vector -> Vector -> Vector -> Picture
+encimar p r a b c = pictures [p a b c, r a b c]
+
 
